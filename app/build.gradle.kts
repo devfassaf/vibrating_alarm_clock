1e671d7fe663420fc19c1784841f6bdbeb4a64c6
Faybish Assaf <assaf.faybish@lendbuzz.com>
2026-08-20 23:24:04 +0300

    release v1.0.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.faybish.vibealarm"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10013
-        versionName = "1.0.13"
+        versionCode = 10014
+        versionName = "1.0.14"
     }
 
     signingConfigs {
